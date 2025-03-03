5221696dadfc98180d3e8f57f714a11d88b30522
begdany <begdany@yandex.ru>
2025-03-03 06:31:58 +0300

    Добавил классы группы и управления группами

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿List<Range> ranges = new List<Range>();
+﻿string inputData = "1-3; 5-8; 2-4";
 
-string inputData = "1-3; 5-8; 2-4";
+GroupManager groupManager = new GroupManager();
+groupManager.AddGroup(0);
 
 string[] dataSets = inputData.Split("; ");
 foreach (var dataSet in dataSets)
@@ -10,13 +11,12 @@ foreach (var dataSet in dataSets)
     int start = int.Parse(bounds[0]);
     int end = int.Parse(bounds[1]);
 
-    ranges.Add(new Range(start, end));
+    groupManager.GetGroup(0).AddRange(new Range(start, end));
 }
 
-foreach (var range in ranges)
-{
-    Console.WriteLine($"Start: {range.Start}, End: {range.End}");
-}
+groupManager.AddGroup(1);
+Range currentRange = groupManager.GetGroup(0).GetRange(1);
+groupManager.GetGroup(1).AddRange(currentRange);
 
 class Range
 {
@@ -27,5 +27,45 @@ class Range
     {
         Start = start;
         End = end;
+    }
+}
+
+class GroupOfRanges
+{
+    public List<Range> Ranges { get; set; }
+
+    public GroupOfRanges()
+    {
+        Ranges = new List<Range>();
+    }
+
+    public void AddRange(Range range)
+    {
+        Ranges.Add(range);
+    }
+
+    public Range GetRange(int rangeID)
+    {
+        return Ranges[rangeID];
+    }
+}
+
+class GroupManager
+{
+    private Dictionary<int, GroupOfRanges> Groups { get; set; }
+
+    public GroupManager()
+    {
+        Groups = new Dictionary<int, GroupOfRanges>();
+    }
+
+    public void AddGroup(int groupID)
+    {
+        Groups[groupID] = new GroupOfRanges();
+    }
+
+    public GroupOfRanges GetGroup(int groupID)
+    {
+        return Groups[groupID];
     }
 }
--- obj/Debug/net9.0/GroupingRanges.AssemblyInfo.cs
+++ obj/Debug/net9.0/GroupingRanges.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GroupingRanges")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09bab303f1a1960622482765052ad0121034882d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GroupingRanges")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GroupingRanges")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
